5ed75814af45eaed22beebe54406b805a5e090ee
Mary Jo Gray <mj81000@gmail.com>
2021-01-07 22:14:44 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(rainbow=["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
+  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, "
+end
